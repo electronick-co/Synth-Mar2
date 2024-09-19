35bec1503f633964fe3d279426b046bbf12d9ed2
Electronick <nicolas.velasquez531@gmail.com>
2024-09-18 21:18:53 -0700

    minor modifications to 003_generate_midi.py

--- ProductionCode/003_generate_midi.py
+++ ProductionCode/003_generate_midi.py
@@ -34,36 +34,48 @@ filtered_availability, filtered_station_data = filter_by_ocean(availability_df, 
 indian_ocean_music_base_dict = create_music_base_dict(filtered_station_data, variable_ranges, filtered_availability, output_octaves = 3)
 
 tempo = 250
-main_key = "C"
-mode = "Mixolydian"
+main_key = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
 
+modes = ['Ionian', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Aeolian', 'Locrian']
+
+
+midi_port_1 = "IAC Driver Bus 1 3"
+midi_port_2 = "IAC Driver Bus 2 4"
+
+# Live Configurable elements
+tempo_range_low = 150
+tempo_range_high = 250
+
+data_to_play = 30
+
+num_instruments_range_low = 3
+num_instruments_range_high = 8
 
 
 # Load the instruments data
 with open('BBCOrchestaInstruments.json', 'r') as f:
     instruments = json.load(f)
 
-
 print("Output ports:", mido.get_output_names())
 print("Input ports:", mido.get_input_names())
 
 
-midi_ports = ["IAC Driver Bus 1 3", 'IAC Driver Bus 2 4']
+midi_ports = [midi_port_1, midi_port_2]
 outports = [mido.open_output(port) for port in midi_ports]
 
 try:
     while True:
-        n_channels = random.randint(3,8)
+        n_channels = random.randint(num_instruments_range_low,num_instruments_range_high)
         print(n_channels)
         channel_data = create_random_channel_data(n_channels, filtered_availability, instruments)
         
         response = send_post_message(MSG_API, str(channel_data))
         print(channel_data)
-        result = create_midi_progression(indian_ocean_music_base_dict, channel_data, main_key, mode)
-        limited_result = [df.iloc[:20] for df in result]
+        result = create_midi_progression(indian_ocean_music_base_dict, channel_data, random.choice(main_key), random.choice(modes),octaves=2)
+        limited_result = [df.iloc[:data_to_play] for df in result]
 
-        tempo = random.randint(200, 250)
+        tempo = random.randint(tempo_range_low, tempo_range_high)
         play_midi_progression(limited_result, tempo, outports, channel_data, instruments)
         
         print("----------")
